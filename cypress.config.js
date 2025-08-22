34faf0826ab8577c3f74ff27379697b6f949fd7b
Liscsb <lissbernardo@gmail.com>
2025-08-21 22:25:47 -0300

    chore: add project configuration files for Cypress and dependencies

---
+++ cypress.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
+  },
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'Cypress Report',
+    embeddedScreenshots: true,
+    inlineAssets: true
+  }
+});
